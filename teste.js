0e29e2a3b267d59769616709340003198c75e415
Rodrigo <160594161+MadebyRodrigo@users.noreply.github.com>
2024-05-30 12:43:02 +0100

    fiz bue

---
+++ teste.js
@@ -0,0 +1,9 @@
+function mostra (id_div) 
+{
+  document.getElementById(id_div).style.visibility = "visible";
+}
+
+function esconde (id_div) 
+{
+  document.getElementById(id_div).style.visibility = "hidden";
+}
