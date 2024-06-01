b63f200673215656699f3729118a17a23e828f80
Rodrigo <160594161+MadebyRodrigo@users.noreply.github.com>
2024-06-01 23:41:47 +0100

    fbdgfdgdf

--- JavaScript/popup.js
+++ JavaScript/popup.js
@@ -1,21 +1,20 @@
-// popup.js
 
+// so many tools so little time
 document.addEventListener("DOMContentLoaded", function() {
   var popupTrigger1 = document.getElementById('popup-trigger1');
   var popup1 = document.getElementById('popup1');
   var closeBtn1 = document.getElementById('close-btn1');
 
-  // Abre o popup ao clicar na imagem
+
   popupTrigger1.addEventListener('click', function() {
     popup1.style.display = 'block';
   });
 
-  // Fecha o popup ao clicar no botão de fechar
   closeBtn1.addEventListener('click', function() {
     popup1.style.display = 'none';
   });
 
-  // Fecha o popup ao clicar fora do conteúdo do popup
+
   window.addEventListener('click', function(event) {
     if (event.target == popup1) {
       popup1.style.display = 'none';
@@ -23,22 +22,23 @@ document.addEventListener("DOMContentLoaded", function() {
   });
 });
 
+// adidas
 document.addEventListener("DOMContentLoaded", function() {
   var popupTrigger2 = document.getElementById('popup-trigger2');
   var popup2 = document.getElementById('popup2');
   var closeBtn2 = document.getElementById('close-btn2');
 
-  // Abre o popup ao clicar na imagem
+
   popupTrigger2.addEventListener('click', function() {
     popup2.style.display = 'block';
   });
 
-  // Fecha o popup ao clicar no botão de fechar
+
   closeBtn2.addEventListener('click', function() {
     popup2.style.display = 'none';
   });
 
-  // Fecha o popup ao clicar fora do conteúdo do popup
+
   window.addEventListener('click', function(event) {
     if (event.target == popup2) {
       popup2.style.display = 'none';
@@ -46,26 +46,26 @@ document.addEventListener("DOMContentLoaded", function() {
   });
 });
 
+// media is just chaos
 document.addEventListener("DOMContentLoaded", function() {
   var popupTrigger3 = document.getElementById('popup-trigger3');
   var popup3 = document.getElementById('popup3');
   var closeBtn3 = document.getElementById('close-btn3');
 
-  // Abre o popup ao clicar na imagem
+
   popupTrigger3.addEventListener('click', function() {
     popup3.style.display = 'block';
   });
 
-  // Fecha o popup ao clicar no botão de fechar
+
   closeBtn3.addEventListener('click', function() {
     popup3.style.display = 'none';
   });
 
-  // Fecha o popup ao clicar fora do conteúdo do popup
+
   window.addEventListener('click', function(event) {
     if (event.target == popup3) {
       popup3.style.display = 'none';
     }
   });
 });
-
--- JavaScript/socials.js
+++ JavaScript/socials.js
@@ -2,10 +2,23 @@ document.addEventListener('DOMContentLoaded', function() {
     const clickableWord = document.querySelector('.socials');
     const extraWord = document.getElementById('linkedin');
     const extraWord2 = document.getElementById('linkedin2');
+    const socialsLink = document.getElementById('socials-link');
 
-    clickableWord.addEventListener('click', function() {
-        this.textContent = '@madebyrodrigo'; // Substitui a palavra
-        extraWord.style.display = 'block'; // Exibe a nova palavra acima
-        extraWord2.style.display = 'block'; // Exibe a nova palavra acima
+    let clicked = false;
+
+    clickableWord.addEventListener('click', function(event) {
+        if (!clicked) {
+            event.preventDefault(); // Impede a navegação no primeiro clique
+            this.textContent = '@madebyrodrigo'; // Substitui a palavra
+            extraWord.style.display = 'block'; // Exibe a nova palavra acima
+            extraWord2.style.display = 'flex'; // Exibe a nova palavra acima
+            clicked = true;
+        }
+    });
+
+    socialsLink.addEventListener('click', function(event) {
+        if (!clicked) {
+            event.preventDefault(); // Impede a navegação no primeiro clique
+        }
     });
 });
