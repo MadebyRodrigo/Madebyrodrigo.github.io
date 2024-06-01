9cf0ecd7a96f3531264507038a7a9cf287d1646a
Rodrigo <160594161+MadebyRodrigo@users.noreply.github.com>
2024-06-01 18:53:24 +0100

    dkhujwabnhdnaw

---
+++ JavaScript/popup.js
@@ -0,0 +1,47 @@
+// popup.js
+
+document.addEventListener("DOMContentLoaded", function() {
+  var popupTrigger1 = document.getElementById('popup-trigger1');
+  var popup1 = document.getElementById('popup1');
+  var closeBtn1 = document.getElementById('close-btn1');
+
+  // Abre o popup ao clicar na imagem
+  popupTrigger1.addEventListener('click', function() {
+    popup1.style.display = 'block';
+  });
+
+  // Fecha o popup ao clicar no botão de fechar
+  closeBtn1.addEventListener('click', function() {
+    popup1.style.display = 'none';
+  });
+
+  // Fecha o popup ao clicar fora do conteúdo do popup
+  window.addEventListener('click', function(event) {
+    if (event.target == popup1) {
+      popup1.style.display = 'none';
+    }
+  });
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+  var popupTrigger2 = document.getElementById('popup-trigger2');
+  var popup2 = document.getElementById('popup2');
+  var closeBtn2 = document.getElementById('close-btn2');
+
+  // Abre o popup ao clicar na imagem
+  popupTrigger2.addEventListener('click', function() {
+    popup2.style.display = 'block';
+  });
+
+  // Fecha o popup ao clicar no botão de fechar
+  closeBtn2.addEventListener('click', function() {
+    popup2.style.display = 'none';
+  });
+
+  // Fecha o popup ao clicar fora do conteúdo do popup
+  window.addEventListener('click', function(event) {
+    if (event.target == popup2) {
+      popup2.style.display = 'none';
+    }
+  });
+});
--- curriculum.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('codigo').addEventListener('click', function() {
-    // URL do PDF que você quer abrir
-    var pdfUrl = 'Elements/Curriculum Vitae.pdf';
-
-    // Abrir o PDF em uma nova guia do navegador
-    window.open(pdfUrl, '_blank');
-});
