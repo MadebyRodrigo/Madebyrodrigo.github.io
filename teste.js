f3e3b0901c37267f91ffa67fe17de02805592743
Rodrigo <160594161+MadebyRodrigo@users.noreply.github.com>
2024-08-08 21:14:43 +0100

    feto

--- teste.js
+++ teste.js
@@ -1,11 +1,18 @@
-
-function getPics() {} //just for this demo
-const imgs = document.querySelectorAll('.gallery img');
-const fullPage = document.querySelector('#fullpage');
-
-imgs.forEach(img => {
-  img.addEventListener('click', function() {
-    fullPage.style.backgroundImage = 'url(' + img.src + ')';
-    fullPage.style.display = 'block';
-  });
+window.addEventListener('DOMContentLoaded', function() {
+  // Verifica a largura do ecrã
+  if (window.innerWidth < 1144) {
+      // Mostra o aviso
+      const warning = document.getElementById('mobile-warning');
+      warning.classList.remove('hidden');
+      
+      // Aguarda 3 segundos e aplica o efeito de fade out
+      setTimeout(() => {
+          warning.classList.add('fade-out');
+      }, 3000);
+      
+      // Aguarda mais 1 segundo para esconder completamente o aviso após o fade out
+      setTimeout(() => {
+          warning.style.display = 'none';
+      }, 4000);
+  }
 });
