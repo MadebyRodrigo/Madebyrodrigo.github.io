a94012d15c9bf7e8feded78b653d86c6a36ef191
Rodrigo <160594161+MadebyRodrigo@users.noreply.github.com>
2024-08-08 15:11:57 +0100

    quase quase

--- JavaScript/menu.js
+++ JavaScript/menu.js
@@ -9,7 +9,6 @@ document.addEventListener('DOMContentLoaded', function () {
         const projectsirLink = document.createElement('a');
         projectsirLink.setAttribute('href', '#projectsir');
         projectsirLink.classList.add('projectsir');
-        projectsirLink.innerHTML = '<h6 class="projectsir">so far</h6>';
         mobileMenu.insertBefore(projectsirLink, mobileMenu.firstChild); // Adiciona projects no início do menu móvel
       } else {
         const projectsirMobile = mobileMenu.querySelector('.projectsir');
